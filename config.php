3804d35dc840dae3bd6c5b45904c3d1c3c83d7b3
Francis Kiarie <kazungu61@outlook.com>
2025-09-12 19:38:20 +0300

    Add config.php for database connection setup

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/vendor/autoload.php'; // load Composer packages
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
